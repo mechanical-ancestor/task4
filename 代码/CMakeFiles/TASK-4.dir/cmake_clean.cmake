06195e74da603187c1611259ac911bcfec74fae9
lan91041 <3179540362@qq.com>
2025-11-26 22:38:26 +0800

    提交考核四 廖文婧

---
+++ 代码/CMakeFiles/TASK-4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TASK-4.dir/test4.cpp.o"
+  "CMakeFiles/TASK-4.dir/test4.cpp.o.d"
+  "TASK-4"
+  "TASK-4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TASK-4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
